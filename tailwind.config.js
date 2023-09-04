eafe288681abba7b423dc5daae88e97e964c1ea3
Aryan Mehesare <99786026+mortal22soul@users.noreply.github.com>
2023-09-04 13:16:32 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    screens: {
+      tab: "860px",
+    },
+    extend: {},
+  },
+  plugins: [],
+};
